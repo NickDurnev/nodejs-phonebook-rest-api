28939f7008634bf13e317aa995a0d3002d50e35c
NickDurnev <92080056+NickDurnev@users.noreply.github.com>
2022-09-24 19:32:02 +0300

    fix: update vercel.json

--- api/index.js
+++ api/index.js
@@ -1,38 +1,19 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-const createError = require("http-errors");
-const catchAsyncErrors = require("../middlewares/errorHandler");
-const { authMiddleware } = require("../middlewares/tokenValidation");
+const app = require("../app");
+const { connection } = require("../db/connection");
 
-const contactsRouter = require("../routes/api/contacts");
-const authRouter = require("../routes/api/auth");
-const usersRouter = require("../routes/api/users");
+require("dotenv").config();
 
-const app = express();
+const PORT = process.env.PORT || 3000;
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const start = async () => {
+  try {
+    await connection();
 
-app.use(logger(formatsLogger));
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
+    app.listen(PORT, () => console.log("Database connection successful"));
+  } catch (error) {
+    console.log(`Server not running. Error message: ${error.message}`);
+    process.exit(1);
+  }
+};
 
-app.use("/api/contacts", contactsRouter);
-app.use("/api/users", authRouter);
-app.use("/users", authMiddleware, usersRouter);
-
-app.use(
-  catchAsyncErrors((req, res, next) => {
-    next(createError(404));
-  })
-);
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    message: error.message,
-  });
-});
-
-module.exports = app;
+start();
---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const createError = require("http-errors");
+const catchAsyncErrors = require("./middlewares/errorHandler");
+const { authMiddleware } = require("./middlewares/tokenValidation");
+
+const contactsRouter = require("./routes/api/contacts");
+const authRouter = require("./routes/api/auth");
+const usersRouter = require("./routes/api/users");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api/contacts", contactsRouter);
+app.use("/api/users", authRouter);
+app.use("/users", authMiddleware, usersRouter);
+
+app.use(
+  catchAsyncErrors((req, res, next) => {
+    next(createError(404));
+  })
+);
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message,
+  });
+});
+
+module.exports = app;
