e974a75c60076dbe55b01691f4246199d466f20a
NickDurnev <92080056+NickDurnev@users.noreply.github.com>
2022-09-25 21:25:54 +0300

    fix: email and surname joi validation

--- middlewares/contactValidation.js
+++ middlewares/contactValidation.js
@@ -5,14 +5,11 @@ module.exports = {
     const schema = Joi.object({
       name: Joi.string().alphanum().min(3).max(25).trim(true).required(),
 
-      surname: Joi.string().alphanum().min(3).max(25).trim(true).required(),
+      surname: Joi.string().alphanum().min(3).max(25).trim(true),
 
       phone: Joi.string().max(20).required(),
 
-      email: Joi.string().email({
-        minDomainSegments: 2,
-        tlds: { allow: ["com", "net"] },
-      }),
+      email: Joi.string().email(),
     });
     const validation = schema.validate(req.body);
     if (validation.error) {
